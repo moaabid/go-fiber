e218493b9cf0ef67de2e34ffec4f7a4258b104c9
moaabid <moaabid153@gmail.com>
2022-04-05 22:07:01 +0530

    first commit

---
+++ lead/lead.go
@@ -0,0 +1,70 @@
+package lead
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
+	"github.com/moaabid/go-fiber-crm-basic/database"
+)
+
+type Lead struct {
+	gorm.Model
+	Name    string `json:"name"`
+	Company string `json:"company"`
+	Email   string `json:"email"`
+	Phone   int    `json:"phone"`
+}
+
+func GetLeads(c *fiber.Ctx) error {
+	db := database.DBconn
+
+	var leads []Lead
+
+	db.Find(&leads)
+
+	return c.JSON(leads)
+}
+
+func GetLead(c *fiber.Ctx) error {
+	id := c.Params("id")
+	db := database.DBconn
+
+	var lead Lead
+
+	db.Find(&lead, id)
+
+	if lead.Name == "" {
+		return c.Status(500).SendString("No lead found with given ID")
+	}
+	return c.JSON(lead)
+
+}
+
+func NewLead(c *fiber.Ctx) error {
+	db := database.DBconn
+
+	lead := new(Lead)
+
+	if err := c.BodyParser(lead); err != nil {
+		return c.Status(503).SendString(err.Error())
+	}
+
+	db.Create(&lead)
+	return c.JSON(lead)
+
+}
+
+func DeleteLead(c *fiber.Ctx) error {
+	id := c.Params("id")
+	db := database.DBconn
+
+	var lead Lead
+
+	db.First(&lead, id)
+
+	if lead.Name == "" {
+		return c.Status(500).SendString("No lead found with given ID")
+	}
+
+	db.Delete(&lead)
+	return c.SendString("Lead Successfully deleted")
+}
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
+	"github.com/moaabid/go-fiber-crm-basic/database"
+	"github.com/moaabid/go-fiber-crm-basic/lead"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/getlead/:id", lead.GetLead)
+	app.Get("/api/v1/getleads", lead.GetLeads)
+	app.Post("/api/v1/addnewlead", lead.NewLead)
+	app.Delete("/api/v1/deletelead/:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+
+	database.DBconn, err = gorm.Open("sqlite3", "lead.db")
+
+	if err != nil {
+		panic("Failed to connect database")
+	}
+	fmt.Println("Database connection opened")
+	database.DBconn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database migrated")
+
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(":3000")
+	defer database.DBconn.Close()
+}
